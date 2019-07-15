c82b0698a3816a9c91a7b60bcaefd6f0d74031fd
wanghaoran5 <309568486@qq.com>
2019-07-15 15:03:44 +0800

    去除多余的空格

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import encodeHtml from './encodeHtml';
 import './fillCharacter.css';
 
 function bindVueFunc(el, binding, vnode) {
-    let value = vnode.test;//vnode.data.attrs.value;
+    let value = vnode.text;//vnode.data.attrs.value;
+    if(vnode.data && vnode.data.domProps && vnode.data.domProps.innerHTML) {
+        value = vnode.data.domProps.innerHTML;
+    }
     if(vnode.children && vnode.children.length === 1) {
-        value = vnode.children[0].text;
+        value = vnode.children[0].text.replace(/^\s(.*)\s$/, '$1');
     }
     let returnValue = format(binding.value, value);
     el.innerHTML = encodeHtml(returnValue);
