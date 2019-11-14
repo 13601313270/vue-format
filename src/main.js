c5cc97b82d946145b60c77df9ad9f36a7cb16a9c
wanghaoran5 <309568486@qq.com>
2019-11-14 11:51:15 +0800

    增加公开方法

--- src/main.js
+++ src/main.js
@@ -21,6 +21,6 @@ export default {
             // 当被绑定的元素插入到 DOM 中时……
             update: bindVueFunc
         });
-        Vue.property.textFormat = format;
+        Vue.prototype.textFormat = format;
     }
 }
