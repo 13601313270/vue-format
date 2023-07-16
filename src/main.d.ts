5d859940570b0fd9b11d62d713cb800bb16a47c9
whr <wang.whr@alibaba-inc.com>
2023-07-17 01:08:26 +0800

    升级到支持vue3

---
+++ src/main.d.ts
@@ -0,0 +1,7 @@
+import { Plugin } from "vue";
+var plugin: Plugin = {
+  install: (props: Plugin[]) => {
+
+  }
+}
+export default plugin
--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import './fillCharacter.css';
 
 function bindVueFunc(el, binding, vnode) {
     let value = vnode.text;//vnode.data.attrs.value;
-    if(vnode.data && vnode.data.domProps && vnode.data.domProps.innerHTML) {
+    if (vnode.data && vnode.data.domProps && vnode.data.domProps.innerHTML) {
         value = vnode.data.domProps.innerHTML;
     }
-    if(vnode.children && vnode.children.length === 1) {
-        value = vnode.children[0].text.replace(/^\s(.*)\s$/, '$1');
+    if (vnode.children && vnode.children.length === 1) {
+        value = vnode.children[0].children.replace(/^\s(.*)\s$/, '$1');
     }
     let returnValue = format(binding.value, value);
     el.innerHTML = encodeHtml(returnValue);
@@ -16,12 +16,8 @@ function bindVueFunc(el, binding, vnode) {
 
 export default {
     install(Vue) {
-        Vue.directive('format', {
-            bind: bindVueFunc,
-            // 当被绑定的元素插入到 DOM 中时……
-            update: bindVueFunc
-        });
-        Vue.prototype.textFormat = (value, code) => {
+        Vue.directive('format', bindVueFunc);
+        Vue.config.globalProperties.$textFormat = (value, code) => {
             return format(code, value).join('');
         };
     }
